ad2ecdf0c401a57c6ea8db833b88ef0299a02c4b
Danilo <danilo@UXs-MacBook-Air.local>
2017-01-20 15:13:12 -0200

    First commit

---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour {
+
+	public GameObject camera;
+	private Vector3 follow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		follow = transform.position;
+		follow.x = camera.transform.position.x;
+		follow.z = camera.transform.position.z;
+		transform.position = follow;
+	}
+}
